6dd1dcd6dda8a36aa77a31fdad210980eca247a6
Marco Dissel <mdissel@users.noreply.github.com>
2020-04-23 11:13:25 +0200

    Add dutch translation for query

---
+++ query/nl.js
@@ -0,0 +1,28 @@
+export default {
+	"Add filter": "Voeg filter toe",
+
+	in: "=",
+	equal: "=",
+	notEqual: "<>",
+	less: "<",
+	greater: ">",
+	greaterOrEqual: ">=",
+	lessOrEqual: "<=",
+	contains: "bevat",
+	notContains: "bevat niet",
+	beginsWith: "begint met",
+	notBeginsWith: "begint niet met",
+	endsWith: "eindigt met",
+	notEndsWith: "eindigt niet met",
+	between: "tussen",
+	notBetween: "niet tussen",
+
+	and: "en",
+	or: "of",
+	Edit: "Bewerken",
+	"Add Filter": "Voeg filter toe",
+	"Add Group": "Voeg groep toe",
+	Delete: "Verwijderen",
+	Apply: "Toepassen",
+	Cancel: "Annuleren",
+};
