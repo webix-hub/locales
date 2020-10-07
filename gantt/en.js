21db35d05c24e465f9a423955668e5fbb351aeea
olistopad <olistopad@xbsoftware.com>
2020-10-07 17:04:36 +0300

    [add] 8.0: Gantt locales

---
+++ gantt/en.js
@@ -0,0 +1,31 @@
+export default {
+	Edit: "Edit",
+	Done: "Done",
+	"Delete task": "Delete task",
+	"Delete link": "Delete link",
+	"The task will be deleted permanently, are you sure?":
+		"The task will be deleted permanently, are you sure?",
+	"The link will be deleted permanently, are you sure?":
+		"The link will be deleted permanently, are you sure?",
+	Predecessors: "Predecessors",
+	Successors: "Successors",
+
+	Title: "Title",
+	"Start date": "Start date",
+	"End date": "End date",
+	Duration: "Duration",
+	Progress: "Progress",
+	Notes: "Notes",
+	"Add task": "Add task",
+	"Add project": "Add project",
+	"(no title)": "(no title)",
+	"Save changes?": "Save changes?",
+
+	"Related tasks": "Related tasks",
+	Task: "Task",
+	Link: "Link",
+	"End to start": "End to start",
+	"Start to start": "Start to start",
+	"End to end": "End to end",
+	"Start to end": "Start to End",
+};
---
+++ gantt/ru.js
@@ -0,0 +1,31 @@
+export default {
+	Edit: "Править",
+	Done: "Готово",
+	"Delete task": "Удалить задачу",
+	"Delete link": "Удалить связь",
+	"The task will be deleted permanently, are you sure?":
+		"Задача будет удалена навсегда, вы уверены?",
+	"The link will be deleted permanently, are you sure?":
+		"Связь будет удалена навсегда, вы уверены?",
+	Predecessors: "Предшествующие",
+	Successors: "Последующие",
+
+	Title: "Название",
+	"Start date": "Начало",
+	"End date": "Конец",
+	Duration: "Длительность",
+	Progress: "Готовность",
+	Notes: "Заметки",
+	"Add task": "Добавить задачу",
+	"Add project": "Добавить проект",
+	"(no title)": "(без названия)",
+	"Save changes?": "Сохранить изменения?",
+
+	"Related tasks": "Связанные задачи",
+	Task: "Задача",
+	Link: "Связь",
+	"End to start": "конец-начало",
+	"Start to start": "начало-начало",
+	"End to end": "конец-конец",
+	"Start to end": "начало-конец",
+};
