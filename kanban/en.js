16741dad2bd630766b5e26b00c94827f88cabda8
olistopad <olistopad@xbsoftware.com>
2022-11-09 11:46:33 +0300

    [add] kanban en locale

---
+++ kanban/en.js
@@ -0,0 +1,23 @@
+export default {
+	"copy" : "Copy",
+	"dnd" : "Drop Files Here",
+	"remove" : "Remove",
+	"save" : "Save",
+	"confirm":"The card will be deleted permanently, are you sure?",
+	"editor":{
+		"add" : "Add card",
+		"assign" : "Assign to",
+		"attachments" : "Attachments",
+		"color" : "Color",
+		"edit" : "Edit card",
+		"status" : "Status",
+		"tags" : "Tags",
+		"text" : "Text",
+		"upload" : "Upload"
+	},
+	"menu":{
+		"copy": "Copy",
+		"edit": "Edit",
+		"remove": "Remove"
+	}
+};
